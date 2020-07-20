a10d8d1fbe1a7513c80250d0affa23550dde6c76
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-07-20 18:17:12 +0900

    static version

--- src/Hello.elm
+++ src/Hello.elm
@@ -20,53 +20,57 @@ main =
 
 view : Model -> Html Msg
 view model =
-    svg
-        [ id "rect"
-        , height "500px"
-        , width "600px"
-        , viewBox "0 0 30 20"
-        ]
-        [ rect [ x "0", y "0", width "30", height "20", fill "#fafafa" ] []
-        , rect
-            [ id "rect"
-            , Mouse.onDown (.clientPos >> DetectDragStart)
-            , Mouse.onMove
-                (\event ->
-                    if isDraggableState model then
-                        ContinueDragging event.clientPos
-
-                    else
-                        NoOp
-                )
-            , Mouse.onUp (\_ -> EndDragging)
-            , Mouse.onLeave (\_ -> EndDragging)
-            , x "0"
-            , y "0"
-            , width "8"
-            , height "10"
-            , fill "#007bff"
-            , transform (transformAttribute model)
-            ]
-            []
-        , rect [ x "18", y "5", width "8", height "10", fill "#888" ] []
-        ]
-
-
-transformAttribute : Model -> String
-transformAttribute model =
     case model of
-        BeingDragged state ->
-            let
-                x =
-                    Tuple.first state.svgTransform
+        State _ svgOuter ->
+            svgOuterView svgOuter
 
-                y =
-                    Tuple.second state.svgTransform
-            in
-            "translate(" ++ String.fromFloat x ++ " " ++ String.fromFloat y ++ ")"
 
-        _ ->
-            ""
+
+-- , rect
+--     [ id "rect"
+--     , Mouse.onDown (.clientPos >> DetectDragStart)
+--     , Mouse.onMove
+--         (\event ->
+--             if isDraggableState model then
+--                 ContinueDragging event.clientPos
+--             else
+--                 NoOp
+--         )
+--     , Mouse.onUp (\_ -> EndDragging)
+--     , Mouse.onLeave (\_ -> EndDragging)
+--     , x "0"
+--     , y "0"
+--     , width "8"
+--     , height "10"
+--     , fill "#007bff"
+--     , transform (transformAttribute model)
+--     ]
+
+
+svgOuterView : SvgOuter -> Html Msg
+svgOuterView svgOuter =
+    svg
+        [ id "svg-outer"
+        , height (String.fromFloat svgOuter.height)
+        , width (String.fromFloat svgOuter.width)
+        , viewBox (viewBoxString svgOuter.viewBox)
+        ]
+        (List.map
+            svgRectView
+            svgOuter.children
+        )
+
+
+svgRectView : SvgRectangle -> Html Msg
+svgRectView svgRect =
+    rect
+        [ id svgRect.id
+        , width (String.fromFloat svgRect.width)
+        , height (String.fromFloat svgRect.height)
+        , transform (transformString svgRect.transform)
+        , fill svgRect.fillColor
+        ]
+        []
 
 
 {-| Msg
@@ -91,126 +95,109 @@ port svgMatrixSubscription : (SvgMatrix -> msg) -> Sub msg
 -}
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
+    ( model, Cmd.none )
 
-        ReceiveSvgMatrix svgMatrix ->
-            case model of
-                Init ->
-                    ( SvgMatrixReceived svgMatrix, Cmd.none )
 
-                DragPositionDetected pos ->
-                    ( DragStarted svgMatrix pos, Cmd.none )
 
-                _ ->
-                    ( model, Cmd.none )
+-- case msg of
+--     NoOp ->
+--         ( model, Cmd.none )
+--     ReceiveSvgMatrix svgMatrix ->
+--         case model of
+--             Init ->
+--                 ( SvgMatrixReceived svgMatrix, Cmd.none )
+--             DragPositionDetected pos ->
+--                 ( DragStarted svgMatrix pos, Cmd.none )
+--             _ ->
+--                 ( model, Cmd.none )
+--     DetectDragStart pos ->
+--         case model of
+--             Init ->
+--                 ( DragPositionDetected pos, Cmd.none )
+--             SvgMatrixReceived matrix ->
+--                 ( DragStarted matrix pos, Cmd.none )
+--             _ ->
+--                 ( model, Cmd.none )
+--     ContinueDragging currentPos ->
+--         case model of
+--             DragStarted svgMatrix startPos ->
+--                 ( BeingDragged
+--                     { svgMatrix = svgMatrix
+--                     , dragStartPos = startPos
+--                     , dragCurrentPos = currentPos
+--                     , svgTransform = ( 0.0, 0.0 )
+--                     }
+--                 , Cmd.none
+--                 )
+--             BeingDragged state ->
+--                 ( BeingDragged
+--                     { state
+--                         | dragCurrentPos = currentPos
+--                         , svgTransform = mult state.svgMatrix state.dragStartPos currentPos
+--                     }
+--                 , Cmd.none
+--                 )
+--             _ ->
+--                 ( model, Cmd.none )
+--     EndDragging ->
+--         case model of
+--             BeingDragged state ->
+--                 ( DragEnded state.svgTransform, Cmd.none )
+--             _ ->
+--                 ( Init, Cmd.none )
 
-        DetectDragStart pos ->
-            case model of
-                Init ->
-                    ( DragPositionDetected pos, Cmd.none )
-
-                SvgMatrixReceived matrix ->
-                    ( DragStarted matrix pos, Cmd.none )
-
-                _ ->
-                    ( model, Cmd.none )
-
-        ContinueDragging currentPos ->
-            case model of
-                DragStarted svgMatrix startPos ->
-                    ( BeingDragged
-                        { svgMatrix = svgMatrix
-                        , dragStartPos = startPos
-                        , dragCurrentPos = currentPos
-                        , svgTransform = ( 0.0, 0.0 )
-                        }
-                    , Cmd.none
-                    )
-
-                BeingDragged state ->
-                    ( BeingDragged
-                        { state
-                            | dragCurrentPos = currentPos
-                            , svgTransform = mult state.svgMatrix state.dragStartPos currentPos
-                        }
-                    , Cmd.none
-                    )
-
-                _ ->
-                    ( model, Cmd.none )
-
-        EndDragging ->
-            case model of
-                BeingDragged state ->
-                    ( DragEnded state.svgTransform, Cmd.none )
-
-                _ ->
-                    ( Init, Cmd.none )
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Init, Cmd.none )
-
-
-mult : SvgMatrix -> ClientPosition -> ClientPosition -> SvgTransform
-mult svgMatrix dragStartPos dragCurrentPos =
-    let
-        dxClient =
-            Tuple.first dragCurrentPos - Tuple.first dragStartPos
-
-        dyClient =
-            Tuple.second dragCurrentPos - Tuple.second dragStartPos
-
-        dxSvg =
-            svgMatrix.a * dxClient + svgMatrix.c * dyClient
-
-        dySvg =
-            svgMatrix.b * dxClient + svgMatrix.d * dyClient
-    in
-    ( dxSvg, dySvg )
+    ( State UnDragged
+        { width = 600
+        , height = 400
+        , viewBox = { minX = 0, minY = 0, width = 30, height = 20 }
+        , matrix = { a = 0, b = 0, c = 0, d = 0, e = 0, f = 0 }
+        , children =
+            [ { id = "dhu", width = 30, height = 20, transform = ( 0, 0 ), fillColor = "#fafafa" }
+            , { id = "bbb", width = 8, height = 10, transform = ( 4, 5 ), fillColor = "#007bff" }
+            , { id = "bbb", width = 8, height = 10, transform = ( 18, 5 ), fillColor = "#888" }
+            ]
+        }
+    , Cmd.none
+    )
 
 
 type Model
-    = Init
-    | SvgMatrixReceived SvgMatrix
-    | DragPositionDetected ClientPosition
-    | DragStarted SvgMatrix ClientPosition
-    | BeingDragged
-        { svgMatrix : SvgMatrix
-        , dragStartPos : ClientPosition
-        , dragCurrentPos : ClientPosition
-        , svgTransform : SvgTransform
+    = State DragState SvgOuter
+
+
+type DragState
+    = UnDragged
+    | Dragged
+        { startPos : ClientPosition
+        , currentPos : ClientPosition
         }
-    | DragEnded SvgTransform
 
-
-isDraggableState : Model -> Bool
-isDraggableState model =
-    case model of
-        DragStarted _ _ ->
-            True
-
-        BeingDragged _ ->
-            True
-
-        _ ->
-            False
-
-
-type alias DragState = {
-    startPos: ClientPosition,
-    currentPos: ClientPosition
-}
 
 type alias ClientPosition =
     ( Float, Float )
 
 
+type alias UserPosition =
+    ( Float, Float )
+
 
 type alias SvgTransform =
     ( Float, Float )
+
+
+transformString : SvgTransform -> String
+transformString svgTransform =
+    let
+        x =
+            String.fromFloat (Tuple.first svgTransform)
+
+        y =
+            String.fromFloat (Tuple.second svgTransform)
+    in
+    "translate (" ++ x ++ " " ++ y ++ ")"
 
 
 type alias SvgMatrix =
@@ -222,7 +209,64 @@ type alias SvgMatrix =
     , f : Float
     }
 
-type SvgElementData = {
-    matrix: SvgMatrix,
-    transform: SvgTransform
-}
+
+type alias SvgOuter =
+    { width : Float
+    , height : Float
+    , viewBox : SvgViewBox
+    , matrix : SvgMatrix
+    , children : List SvgRectangle
+    }
+
+
+mult : SvgMatrix -> ClientPosition -> UserPosition
+mult inverse clientPos =
+    let
+        clientX =
+            Tuple.first clientPos
+
+        clientY =
+            Tuple.second clientPos
+
+        userX =
+            inverse.a * clientX + inverse.c * clientY + inverse.e
+
+        userY =
+            inverse.b * clientX + inverse.d * clientY + inverse.f
+    in
+    ( userX, userY )
+
+
+type alias SvgRectangle =
+    { id : String
+    , height : Float
+    , width : Float
+    , transform : SvgTransform
+    , fillColor : String
+    }
+
+
+type alias SvgViewBox =
+    { minX : Float
+    , minY : Float
+    , width : Float
+    , height : Float
+    }
+
+
+viewBoxString : SvgViewBox -> String
+viewBoxString viewBox =
+    let
+        minX =
+            String.fromFloat viewBox.minX
+
+        minY =
+            String.fromFloat viewBox.minY
+
+        width =
+            String.fromFloat viewBox.width
+
+        height =
+            String.fromFloat viewBox.height
+    in
+    minX ++ " " ++ minY ++ " " ++ width ++ " " ++ height
